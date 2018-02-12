21c15da00a05435a54a401a7ad136072965351b9
davidlambis <davidlambis261@gmail.com>
2018-02-12 18:14:06 -0500

    AppDatabase dbflow

---
+++ app/src/main/java/com/interedes/agriculturappv2/config/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.interedes.agriculturappv2.config
+
+import com.raizlabs.android.dbflow.annotation.Database
+
+@Database(name = AppDatabase.NAME, version = AppDatabase.VERSION, generatedClassSeparator = "_")
+object AppDatabase {
+    const val NAME: String = ""
+    const val VERSION: Int = 1
+}
